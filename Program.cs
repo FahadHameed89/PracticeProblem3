5fc496b96e32bcd04f02dd45cc3c5de15fc00a5b
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-09 17:51:44 -0800

    Fixed crashing bug

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace PracticeProblem3
              int birthYear = currentYear-myAge;
 
              Console.WriteLine($"Based on the fact that it's {currentYear}, your birth year must be {birthYear} assuming your birthday has already passed!");
+             Console.ReadLine();
         }
     }
 }
